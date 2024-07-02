7923ea8777f25973748ab0342a60e0bed96e5784
MOHFEBRIAN <mpebrian343@gmail.com>
2024-07-02 13:52:45 -0700

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,39 @@
+function cekKhodam(event) {
+    event.preventDefault();
+    let nama = $('#search').val();
+    if (nama === '') {
+        alert('Masukkan nama terlebih dahulu!');
+    } else {
+        let status = [
+            'isi',
+            'kosong',
+        ]
+        const randomStatus = Math.floor(Math.random() * status.length);
+        if (randomStatus === 0) {
+            let khodam = [
+                'Jinn',
+                'Syaitan',
+                'Malaikat',
+                'Peri',
+                'Naga',
+                'Tobrut',
+                'Angling Dharma',
+                'Jin HItam',
+                'Iblis Bermata Satu',
+                'Kuntilanak',
+                'Pocong'
+            ];
+            const random = Math.floor(Math.random() * khodam.length);
+            $('#result').html(`${nama.toUpperCase()} - Khodam Kamu (${khodam[random]})`);
+            $('#search').val('');
+        }
+        else {
+            $('#result').html(`${nama.toUpperCase()} - KOSONG`);
+            $('#search').val('');
+        }
+    }
+}
+
+$(document).ready(function () {
+    $('#btn-search').click(cekKhodam);
+});
